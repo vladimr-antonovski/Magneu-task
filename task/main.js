791d2eef441c73090dd2d7c421839529b5b92613
vladimir antonovski <vladimir.antonovski@gmail.com>
2023-04-03 17:04:57 +0200

    initialization

---
+++ task/main.js
@@ -0,0 +1,13 @@
+const getData = () => {
+  fetch("GamblersKnowAPI.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+
+
+
+    
+};
+
+getData();
