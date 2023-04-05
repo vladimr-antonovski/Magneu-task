02b6bb375cc47a0ed415dad077762112c1720ff5
vladimir antonovski <vladimir.antonovski@gmail.com>
2023-04-05 23:25:46 +0200

    styling

--- task/main.js
+++ task/main.js
@@ -2,12 +2,19 @@ const getData = () => {
   fetch("GamblersKnowAPI.json")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-    });  
+
+
+      let array = data;
+      console.log(array);
+      
+      let bgColor = array.colors.background_color;
+      const navbar = document.querySelector('.nav')
+      navbar.style.backgroundColor = bgColor;
+
+      let headerColor = array.colors.header_text_color;
+      let heading =  document.querySelector('.heading')
+      heading.style.color = headerColor;
+
+    });
 };
 getData();
-
-
-
-
-
