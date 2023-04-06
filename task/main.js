db32cf9bd25997f5066d220749f44ba59687daae
vladimir antonovski <vladimir.antonovski@gmail.com>
2023-04-06 23:05:07 +0200

    styling

--- task/main.js
+++ task/main.js
@@ -17,8 +17,28 @@ navbar.append(logoImg);
 
 let middleDiv = document.querySelector('.middle_inner')
 let headerTxt = document.querySelector('.header');
-headerTxt.innerHTML = array.header + array.colors.header_text_color;
-middleDiv.append(headerTxt)
+headerTxt.style.color = array.colors.header_text_color
+headerTxt.innerHTML = array.header
+
+let subheader = document.querySelector(".subheader")
+subheader.innerHTML = array.subheader;
+subheader.style.color = array.colors.subheader_text_color;
+
+let underline = document.querySelector('.underline')
+underline.style.color = array.colors.underline_color;
+
+let text = document.createElement('p')
+text.innerText = "Add your name and email below to gain instant access to our exclusive bonuses"
+text.style.color = array.colors.header_text_color;
+
+middleDiv.append(headerTxt, subheader, underline, text)
+
+let terms_conditions = document.querySelector('.terms_conditions')
+terms_conditions.style.backgroundColor = array.colors.background_color
+
+const waring = document.querySelector('.disclaimer');
+waring.style.color = array.colors.button_color;
+
 
 
 
