29131752bedde5d98b40ac8e0f0782993b79ee90
vladimir antonovski <vladimir.antonovski@gmail.com>
2023-04-13 10:37:47 +0200

    format page 2

--- task/page2.js
+++ task/page2.js
@@ -1,35 +1,30 @@
-
 const fetchData = () => {
-    fetch("GamblersKnowAPI.json")
-      .then((response) => response.json())
-      .then((data) => {
+  fetch("GamblersKnowAPI.json")
+    .then((response) => response.json())
+    .then((data) => {
+      let api = data;
 
-        let api = data;
+      const Navbar = document.querySelector("#navbar");
+      Navbar.style.backgroundColor = api.colors.background_color;
 
-    const Navbar = document.querySelector('#navbar');
-    Navbar.style.backgroundColor = api.colors.background_color;
+      const img_logo = document.createElement("img");
+      img_logo.src = api.page_logo;
+      img_logo.classList.add("logo");
+      Navbar.append(img_logo);
 
-    const img_logo = document.createElement("img");
-    img_logo.src = api.page_logo;
-    img_logo.classList.add('logo');
-    Navbar.append(img_logo)
+      const grey_container = document.querySelector(".grey_container");
+      grey_container.style.backgroundColor = "#474747";
 
-    const grey_container = document.querySelector('.grey_container')
-    grey_container.style.backgroundColor = "#474747"
+      const h1 = document.createElement("h1");
+      h1.innerHTML = api.header;
+      h1.style.color = api.colors.header_text_color;
 
-    const h1 = document.createElement("h1");
-    h1.innerHTML = api.header;
-    h1.style.color = api.colors.header_text_color;
+      const subText = document.createElement("p");
+      subText.innerHTML = api.subheader;
+      subText.style.color = api.colors.header_text_color;
+      subText.classList.add("sub_header");
 
-    const subText = document.createElement('p');
-    subText.innerHTML = api.subheader;
-    subText.style.color = api.colors.header_text_color;
-    subText.classList.add('sub_header')
-
-
-    grey_container.append(h1, subText)
-
-
+      grey_container.append(h1, subText);
     });
 };
-fetchData();  
+fetchData();
